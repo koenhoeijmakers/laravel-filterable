2f9b7b6995e8348c150fcd12bc17d8d649b12f6e
Koen Hoeijmakers <koen@hoeijmakers.me>
2018-05-28 20:23:25 +0200

    clarify configs

--- config/filterable.php
+++ config/filterable.php
@@ -5,15 +5,61 @@ use KoenHoeijmakers\LaravelFilterable\Filters\Like;
 use KoenHoeijmakers\LaravelFilterable\Sorters\OrderBy;
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default
+    |--------------------------------------------------------------------------
+    |
+    | These keys assign the default filter and sorter for when no other
+    | sorter or filter was assigned.
+    |
+    */
     'default' => [
         'filter' => Like::class,
         'sorter' => OrderBy::class,
     ],
-    'filters'  => [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filters
+    |--------------------------------------------------------------------------
+    |
+    | These are filters that can be assigned by a string value, for easy
+    | access.
+    |
+    */
+    'filters' => [
         'equal' => Equal::class,
         'like'  => Like::class,
     ],
-    'sorters'  => [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sorters
+    |--------------------------------------------------------------------------
+    |
+    | These are sorters that can be assigned by a string value, for easy
+    | access.
+    |
+    */
+    'sorters' => [
         'order_by' => OrderBy::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Keys
+    |--------------------------------------------------------------------------
+    |
+    | These are the keys for certain parameters in the request.
+    |
+    | Dot notation is allowed.
+    |
+    */
+    'keys'    => [
+        'filter'    => 'q',
+        'sort_by'   => 'sortBy',
+        'sort_desc' => 'sortDesc',
     ],
 ];
