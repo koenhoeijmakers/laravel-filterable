0ad04005f04e006591e7e2838747e9312b65f997
KoenHoeijmakers <koen@hoeijmakers.me>
2019-04-02 18:55:41 +0200

    add tests

---
+++ tests/TestCase.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace KoenHoeijmakers\LaravelFilterable\Tests;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase();
+    }
+
+    protected function setUpDatabase()
+    {
+        Schema::create('test_models', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+}
+
+class TestModel extends Model
+{
+    protected $fillable = ['name'];
+}
